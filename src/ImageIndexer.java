07882251438f11fe6f5061faf96ba7ce372f7c13
huangyf530 <lancerken530@outlook.com>
2019-06-21 00:03:16 +0800

    handle html

--- src/ImageIndexer.java
+++ src/ImageIndexer.java
@@ -1,12 +1,8 @@
 import java.io.*;
 import java.nio.file.Paths;
-import java.util.*;
-
-
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.*;
 import org.apache.lucene.search.similarities.BM25Similarity;
-import org.w3c.dom.*;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Document;
@@ -15,19 +11,21 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
+import org.w3c.dom.*;
+import org.jsoup.*;
 
 import javax.xml.parsers.*; 
 
 public class ImageIndexer {
 	private Analyzer analyzer; 
     private IndexWriter indexWriter;
-    private float averageLength=1.0f;
+    private double averageLength=1.0f;
     
     public ImageIndexer(String indexDir){
-    	analyzer = new IKAnalyzer();
+    	analyzer = new StandardAnalyzer();
     	try{
     		IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+    		iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
     		Directory dir = FSDirectory.open(Paths.get(indexDir));
     		iwc.setSimilarity(new BM25Similarity());
     		indexWriter = new IndexWriter(dir,iwc);
@@ -60,23 +58,23 @@ public class ImageIndexer {
 			org.w3c.dom.Document doc = db.parse(new File(filename));
 			NodeList nodeList = doc.getElementsByTagName("pic");
 			for(int i=0;i<nodeList.getLength();i++){
-				Node node=nodeList.item(i);
+				org.w3c.dom.Node node=nodeList.item(i);
 				NamedNodeMap map=node.getAttributes();
-				Node locate=map.getNamedItem("locate");
-				Node bigClass=map.getNamedItem("bigClass");
-				Node smallClass=map.getNamedItem("smallClass");
-				Node query=map.getNamedItem("query");
+				org.w3c.dom.Node locate=map.getNamedItem("locate");
+				org.w3c.dom.Node bigClass=map.getNamedItem("bigClass");
+				org.w3c.dom.Node smallClass=map.getNamedItem("smallClass");
+				org.w3c.dom.Node query=map.getNamedItem("query");
 				String absString=bigClass.getNodeValue()+" "+smallClass.getNodeValue()+" "+query.getNodeValue();
-//				Document document  =   new  Document();
-//				Field PicPathField  =   new StringField( "picPath" ,locate.getNodeValue(),Field.Store.YES);
-//				Field abstractField  =   new TextField("abstract" ,absString,Field.Store.YES);
-//				averageLength += absString.length();
-//				document.add(PicPathField);
-//				document.add(abstractField);
-//				indexWriter.addDocument(document);
-//				if(i%10000==0){
-//					System.out.println("process "+i);
-//				}
+				Document document  =   new  Document();
+				Field PicPathField  =   new StringField( "picPath" ,locate.getNodeValue(),Field.Store.YES);
+				Field abstractField  =   new TextField("abstract" ,absString,Field.Store.YES);
+				averageLength += absString.length();
+				document.add(PicPathField);
+				document.add(abstractField);
+				indexWriter.addDocument(document);
+				if(i%10000==0){
+					System.out.println("process "+i);
+				}
 				//TODO: add other fields such as html title or html content
 			}
 //			averageLength /= indexWriter.numDocs();
@@ -84,6 +82,31 @@ public class ImageIndexer {
 //			System.out.println("total "+indexWriter.numDocs()+" documents");
 			indexWriter.close();
 		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * <p>
+	 * htmlfile
+	 *
+	 */
+	private void indexHtmlFile(String filename){
+		try{
+			org.jsoup.nodes.Document doc = Jsoup.parse(new File(filename), "utf-8");
+//			System.out.println("Title is:"+doc.title());
+//			System.out.println("Body is:"+doc.body().text());
+			String title = doc.title();
+			String content = doc.body().text();
+			Document document  = new Document();
+			Field contentField  = new TextField("content" ,content,Field.Store.YES);
+			Field titleField = new TextField("title", title, Field.Store.YES);
+			document.add(contentField);
+			document.add(titleField);
+			indexWriter.addDocument(document);
+			averageLength += content.length();
+		}
+		catch (Exception e){
 			e.printStackTrace();
 		}
 	}
@@ -107,7 +130,8 @@ public class ImageIndexer {
 	}
 	public static void main(String[] args) {
 		ImageIndexer indexer=new ImageIndexer("forIndex/index");
-		indexer.indexSpecialFile("input/sogou-utf8.xml");
+		indexer.indexHtmlFile("input/index.html");
+//		indexer.indexSpecialFile("input/sogou-utf8.xml");
 //		indexer.saveGlobals("forIndex/global.txt");
 	}
 }
